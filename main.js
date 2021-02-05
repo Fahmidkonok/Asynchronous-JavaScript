3b6c7948a4ef8e702f79cd86c3e5176043ef7cfa
Fahmidkonok <targetzone555@gmail.com>
2021-02-05 22:07:14 +0600

    good
    its good

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+//call back function
+
+/*
 console.log(1);
 console.log(2);
 
@@ -7,5 +10,17 @@ setTimeout(() =>{
 
 console.log(3);
 console.log(4);
+*/
+//HTTP requests
 
+const request = new XMLHttpRequest();
 
+request.addEventListener('readystatechange', () => {
+    // console.log(request, request.readyState);
+    if(request.readyState === 4){
+        console.log(request, request.responseText);
+    }
+});
+
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+request.send();
